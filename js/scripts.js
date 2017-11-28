8261b7992d130a4c67179a20415ae1927ea67053
Sam Gespass and Zach Evans <zach@>
2017-11-28 10:07:28 -0800

    Add jquery that highlights paragraphs.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#p1").click(function() {
+    $("#p1").toggleClass("selected");
+  });
+  $("#p2").click(function() {
+    $("#p2").toggleClass("selected");
+  });
+  $("#p3").click(function() {
+    $("#p3").toggleClass("selected");
+  });
+  $("#p4").click(function() {
+    $("#p4").toggleClass("selected");
+  });
+});
